81a5d2def20fc6c9901fec3f140d5ce07ab892ca
chihabMg <chihab.mg.me@gmail.com>
2023-05-01 17:33:39 +0100

    added social login/registration page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -2,12 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import Button from "~/components/ui/Button";
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
-const LoginPage = () => {
+const RegistrationPage = () => {
   return (
     <>
       <Head>
-        <title>login page</title>
+        <title>Registration page</title>
       </Head>
       <main className="flex flex justify-center pt-[100px]   ">
         <div className="flex w-full max-w-[400px] flex-col gap-4 ">
@@ -19,25 +20,26 @@ const LoginPage = () => {
               height={64}
               className="absolute left-[20px] h-7 w-7 rounded-full"
             />
-            <span>login with google</span>
+            <span>register with google</span>
           </Button>
           <Button className="hover:ring-2-blue-400 relative flex h-14 items-center justify-center gap-2 rounded-full !bg-blue-600 capitalize hover:ring-2">
             <Image
-              alt="google image"
+              alt="facebook image"
               src="/images/social/facebook.png"
               width={64}
               height={64}
               className="absolute left-[20px] h-7 w-7 rounded-full"
             />
-            <span>login with facebook</span>
+            <span>register with facebook</span>
           </Button>
           <Button className="relative flex h-14 items-center justify-center gap-2  rounded-full capitalize hover:ring-2 hover:ring-primary">
             <EnvelopeIcon className="absolute left-[20px] h-6 w-6 text-white" />
-            <span>login with magic link</span>
+            <span>register with magic link</span>
           </Button>
         </div>
       </main>
     </>
   );
 };
-export default LoginPage;
+
+export default RegistrationPage;
