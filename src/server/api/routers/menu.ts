f219f968972996f5984aa696980ba9ba80efdb3a
chihabMg <chihab.mg.me@gmail.com>
2023-05-12 09:51:38 +0100

    added try catch block in the menu route

--- src/server/api/routers/menu.ts
+++ src/server/api/routers/menu.ts
@@ -12,10 +12,25 @@ import { generateUploadURL } from "~/helpers/s3";
 
 export const menuRouter = createTRPCRouter({
   getAllMenuItems: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.menuItem.findMany();
+    try {
+      return ctx.prisma.menuItem.findMany();
+    } catch (err) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "something went wrong",
+      });
+    }
   }),
   getAllCategories: publicProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.menuCategory.findMany();
+    try {
+      return ctx.prisma.menuCategory.findMany();
+    } catch (err) {
+      console.error(err);
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "something went wrong",
+      });
+    }
   }),
   addCategory: protectedProcedure
     .input(
@@ -25,17 +40,25 @@ export const menuRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      if (ctx.session.user.role != "admin")
-        throw new TRPCError({
-          code: "UNAUTHORIZED",
-          message: "you dont have the permessions to access this route",
+      try {
+        if (ctx.session.user.role != "admin")
+          throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "you dont have the permessions to access this route",
+          });
+        return ctx.prisma.menuCategory.create({
+          data: {
+            name: input.name,
+            image: input.imageURL,
+          },
         });
-      return ctx.prisma.menuCategory.create({
-        data: {
-          name: input.name,
-          image: input.imageURL,
-        },
-      });
+      } catch (err) {
+        console.error(err);
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "something went wrong",
+        });
+      }
     }),
   getMenuItemsByCategory: publicProcedure
     .input(
@@ -44,12 +67,20 @@ export const menuRouter = createTRPCRouter({
       })
     )
     .query(({ ctx, input }) => {
-      return ctx.prisma.menuCategory.findFirst({
-        where: { id: input.id },
-        select: {
-          items: true,
-        },
-      });
+      try {
+        return ctx.prisma.menuCategory.findFirst({
+          where: { id: input.id },
+          select: {
+            items: true,
+          },
+        });
+      } catch (err) {
+        console.error(err);
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "something went wrong",
+        });
+      }
     }),
   addItemToTheMenu: protectedProcedure
     .input(
@@ -62,40 +93,47 @@ export const menuRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      console.log(input);
-      const id = ctx.session.user.id;
-      const user = await findUserById(id, ctx.prisma);
-      checkIsARestaurant(user);
-      const category = await ctx.prisma.menuCategory.findFirst({
-        where: {
-          id: input.categoryId,
-        },
-      });
-      if (!category)
-        return new TRPCError({
-          code: "BAD_REQUEST",
-          message: "invalid category id",
+      try {
+        const id = ctx.session.user.id;
+        const user = await findUserById(id, ctx.prisma);
+        checkIsARestaurant(user);
+        const category = await ctx.prisma.menuCategory.findFirst({
+          where: {
+            id: input.categoryId,
+          },
         });
-      const restuarant = await ctx.prisma.restaurant.findFirst({
-        where: {
-          userId: user.id,
-        },
-      });
-      if (!restuarant)
+        if (!category)
+          return new TRPCError({
+            code: "BAD_REQUEST",
+            message: "invalid category id",
+          });
+        const restuarant = await ctx.prisma.restaurant.findFirst({
+          where: {
+            userId: user.id,
+          },
+        });
+        if (!restuarant)
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "invalid restaurant",
+          });
+        return ctx.prisma.menuItem.create({
+          data: {
+            name: input.name,
+            price: input.price,
+            description: input.description,
+            userId: user.id,
+            menuCategoryId: category.id,
+            restaurantId: restuarant.id,
+            image: input.imageURL,
+          },
+        });
+      } catch (err) {
+        console.error(err);
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: "invalid restaurant",
+          message: "something went wrong",
         });
-      return ctx.prisma.menuItem.create({
-        data: {
-          name: input.name,
-          price: input.price,
-          description: input.description,
-          userId: user.id,
-          menuCategoryId: category.id,
-          restaurantId: restuarant.id,
-          image: input.imageURL,
-        },
-      });
+      }
     }),
 });
