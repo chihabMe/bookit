e5235a21a21390e4695933cca346dd58b2696fd6
chihabMg <chihab.mg.me@gmail.com>
2023-05-12 12:44:52 +0100

    added menu item page

--- src/context/cart.context.tsx
+++ src/context/cart.context.tsx
@@ -10,9 +10,9 @@ interface ICartContext {
   getTotal: () => number;
   items: ICartItem[];
   cleare: () => void;
-  addItem: (item: ICartItem) => void;
+  addItem: (item: ICartItem, qt?: number) => void;
   removeItem: (id: string) => void;
-  increaseItem: (id: string) => void;
+  increaseItem: (id: string, qt?: number) => void;
   getNumberOfItems: () => number;
   decreaseItem: (id: string) => void;
 }
@@ -31,13 +31,17 @@ export const cartContext = createContext(initialState);
 export const CartContextProvider = ({ children }: { children: ReactNode }) => {
   const [items, setItems] = useState(initialState.items);
   const getNumberOfItems = () => items.length;
-  const increaseItem = (id: string) => {
+  const increaseItem = (id: string, qt?: number) => {
     setItems((prev) =>
-      prev.map((item) =>
-        item.id != id ? item : { ...item, quantity: item.quantity + 1 }
-      )
+      prev.map((item) => {
+        const total = +(qt ?? 1);
+        return item.id != id
+          ? item
+          : { ...item, quantity: item.quantity + total };
+      })
     );
   };
+
   const decreaseItem = (id: string) => {
     setItems(
       (prev) =>
@@ -51,13 +55,14 @@ export const CartContextProvider = ({ children }: { children: ReactNode }) => {
   const removeItem = (id: string) => {
     setItems((prev) => prev.filter((item) => item.id != id));
   };
-  const addItem = (item: ICartItem) => {
+  const addItem = (item: ICartItem, qt?: number) => {
     if (items.map((item) => item.id).includes(item.id)) {
-      increaseItem(item.id);
+      increaseItem(item.id, qt);
     } else {
-      setItems((prev) => [...prev, item]);
+      setItems((prev) => [...prev, { ...item, quantity: qt ?? 1 }]);
     }
   };
+  useEffect(() => {}, [items]);
   const cleare = () => {
     setItems([]);
   };
--- src/pages/menu/[slug].tsx
+++ src/pages/menu/[slug].tsx
@@ -2,18 +2,230 @@ import { MenuItem } from "@prisma/client";
 import { GetServerSideProps, NextPage } from "next";
 import { prisma } from "~/server/db";
 import Head from "next/head";
+import Image from "next/image";
+import Button from "~/components/ui/Button";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import useCart from "~/hooks/useCart";
+import { ICartItem } from "~/context/cart.context";
+import { ChangeEvent, useState } from "react";
 
 interface MenuItemPageProps {
   menuItem: MenuItem & { price: string; createdAt: string; updatedAt: string }; // Convert price to a JSON serializable type
 }
 
 const MenuItemPage: NextPage<MenuItemPageProps> = ({ menuItem }) => {
+  const [qt, setQt] = useState(1);
+  const { addItem } = useCart();
+  const handleQtChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const quantity = parseInt(e.target.value);
+    setQt(quantity);
+  };
+
   return (
     <>
       <Head>
         <title>{menuItem.name}</title>
       </Head>
-      <main className="text-gray-400">{menuItem.description}</main>
+      <main className="text-gray-400">
+        <section className="bg-blueGray-100 rounded-b-10xl overflow-hidden pb-24 pt-12">
+          <div className="container mx-auto px-4">
+            <div className="-mx-4 flex flex-wrap">
+              <div className="mb-16 w-full px-4 lg:mb-0 lg:w-1/2">
+                <div className="-mx-4 flex flex-wrap items-center justify-between lg:items-start lg:justify-start xl:items-center">
+                  <div className="flex w-full min-w-max items-center justify-center px-4 text-center sm:w-auto sm:flex-col">
+                    <a
+                      className="hover:text-darkBlueGray-400 mr-4 inline-block -rotate-90 transform sm:mb-12 sm:mr-0 sm:transform-none"
+                      href="#"
+                    ></a>
+                    <a className="h-30 mb-4 mr-2 block sm:mr-0" href="#">
+                      <Image
+                        className="h-full w-full"
+                        width={700}
+                        height={700}
+                        src={menuItem.image ?? ""}
+                        alt={` ${menuItem.name}  image`}
+                      />
+                    </a>
+                    <a
+                      className="h-30 mb-4 mr-2 hidden sm:mr-0 sm:block"
+                      href="#"
+                    >
+                      <img
+                        className="h-full w-full"
+                        src="uinel-assets/images/ecommerce-product-info/placeholder2.png"
+                        alt=""
+                      />
+                    </a>
+                    <a
+                      className="h-30 border-blueGray-500 mb-4 mr-2 hidden rounded-xl border-2 sm:mr-0 sm:block"
+                      href="#"
+                    >
+                      <img
+                        className="h-full w-full"
+                        src="uinel-assets/images/ecommerce-product-info/placeholder4.png"
+                        alt=""
+                      />
+                    </a>
+                    <a
+                      className="h-30 mb-4 mr-4 block sm:mb-12 sm:mr-0"
+                      href="#"
+                    >
+                      <img
+                        className="h-full w-full"
+                        src="uinel-assets/images/ecommerce-product-info/placeholder3.png"
+                        alt=""
+                      />
+                    </a>
+                  </div>
+                  <div className="w-full px-4 sm:w-9/12">
+                    <img
+                      className="mb-5"
+                      src="uinel-assets/images/ecommerce-product-info/ph-photo1.png"
+                      alt=""
+                    />
+                    <p className="text-sm text-gray-300">
+                      Roll over image to zoom in
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="mb-6 max-w-md">
+                  <h2 className="font-heading mb-4 mt-6 text-5xl font-medium text-title dark:text-title-dark md:text-7xl lg:text-8xl">
+                    {menuItem.name}
+                  </h2>
+                  <p className="mb-6 flex items-center">
+                    <span className="mr-2 text-sm font-medium text-blue-500 text-primary">
+                      $
+                    </span>
+                    <span className="text-3xl font-medium text-blue-500 text-primary ">
+                      {menuItem.price}
+                    </span>
+                  </p>
+                  <h1 className="py-2 text-xl font-medium capitalize text-title dark:text-title-dark">
+                    description:
+                  </h1>
+                  <p className="px-2  text-lg text-sm text-text dark:text-text-dark">
+                    {menuItem.description}
+                  </p>
+                </div>
+                <div className="mb-6 flex items-center">
+                  <div className="mr-4 inline-flex">
+                    <button className="mr-1"></button>
+                    <button className="mr-1">
+                      <svg
+                        width="20"
+                        height="20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M20 7.91679H12.4167L10 0.416779L7.58333 7.91679H0L6.18335 12.3168L3.81668 19.5835L10 15.0835L16.1834 19.5835L13.8167 12.3168L20 7.91679Z"
+                          fill="#C1C9D3"
+                        ></path>
+                      </svg>
+                    </button>
+                    <button className="mr-1">
+                      <svg
+                        width="20"
+                        height="20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M20 7.91679H12.4167L10 0.416779L7.58333 7.91679H0L6.18335 12.3168L3.81668 19.5835L10 15.0835L16.1834 19.5835L13.8167 12.3168L20 7.91679Z"
+                          fill="#C1C9D3"
+                        ></path>
+                      </svg>
+                    </button>
+                    <button className="mr-1">
+                      <svg
+                        width="20"
+                        height="20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M20 7.91679H12.4167L10 0.416779L7.58333 7.91679H0L6.18335 12.3168L3.81668 19.5835L10 15.0835L16.1834 19.5835L13.8167 12.3168L20 7.91679Z"
+                          fill="#C1C9D3"
+                        ></path>
+                      </svg>
+                    </button>
+                    <button>
+                      <svg
+                        width="20"
+                        height="20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M20 7.91679H12.4167L10 0.416779L7.58333 7.91679H0L6.18335 12.3168L3.81668 19.5835L10 15.0835L16.1834 19.5835L13.8167 12.3168L20 7.91679Z"
+                          fill="#C1C9D3"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                  <span className="text-md text-gray-400">4.59</span>
+                </div>
+                <div className="mb-10">
+                  <h4 className="font-heading mb-3 font-medium text-text dark:text-text-dark">
+                    quantity
+                  </h4>
+                  <input
+                    value={qt}
+                    onChange={handleQtChange}
+                    className="w-24 rounded-xl border-2 border-primary bg-white px-3 py-2 text-center text-primary outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
+                    type="text"
+                    placeholder="1"
+                  />
+                </div>
+                <div className="-mx-2 mb-12 flex flex-wrap">
+                  <Button
+                    onClick={() => {
+                      addItem(
+                        {
+                          ...menuItem,
+                          price: menuItem.price.toString(),
+                        } as ICartItem,
+                        qt
+                      );
+                    }}
+                    className="px-8 py-4"
+                  >
+                    add to cart
+                  </Button>
+                </div>
+                <div>
+                  <h4 className="font-heading mb-6 font-medium">
+                    More information
+                  </h4>
+                  <button className="border-blueGray-200 hover:border-blueGray-300 mb-4 flex w-full items-center justify-between rounded-2xl border-2 py-4 pl-6 pr-6 leading-7 lg:pl-12">
+                    <h3 className="font-heading text-lg font-medium">
+                      Shipping &amp; returns
+                    </h3>
+                    <span>
+                      <svg
+                        width="12"
+                        height="8"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M10.4594 0.289848C10.8128 -0.096616 11.3841 -0.096616 11.7349 0.289848C12.0871 0.676312 12.0897 1.30071 11.7349 1.68718L6.63794 7.21015C6.28579 7.59662 5.71584 7.59662 5.36108 7.21015L0.264109 1.68718C-0.0880363 1.30215 -0.0880363 0.676312 0.264109 0.289848C0.617558 -0.096616 1.18882 -0.096616 1.53966 0.289848L6.00147 4.81927L10.4594 0.289848Z"
+                          fill="black"
+                        ></path>
+                      </svg>
+                    </span>
+                  </button>
+                  <button className="border-blueGray-200 hover:border-blueGray-300 flex w-full items-center justify-between rounded-2xl border-2 py-4 pl-6 pr-6 leading-7 lg:pl-12">
+                    <h3 className="font-heading text-lg font-medium">
+                      Product details
+                    </h3>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
